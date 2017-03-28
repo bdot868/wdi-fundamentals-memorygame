a013901de394e1d4e2f7d7983f9c4b1b0868d56c
bdot868 <brandoncarter100@gmail.com>
2017-03-28 00:07:32 -0700

    created the cards using javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,19 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if(cardOne === cardTwo && cardThree === cardFour){
+/*if(cardOne === cardTwo && cardThree === cardFour){
   alert('You found a Match')
 } else {
   alert('Sorry, try again')
 }
+*/
+function createCards(){
+  gameBoard = document.getElementById('game-board')
+
+  for (var i = 0; i < 4; i++) {
+    cards = document.createElement('div')
+    cards.className = "card";
+    gameBoard.appendChild(cards)
+  }
+}
+createCards()
